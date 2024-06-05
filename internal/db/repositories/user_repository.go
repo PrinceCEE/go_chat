f805a152cb289d356ab69b33bab21b6906b0d5f3
Chimezie Edeh <princecee15@gmail.com>
2024-06-05 18:38:11 +0100

    feat: add userservice and user repository implementation

--- internal/db/repositories/user_repository.go
+++ internal/db/repositories/user_repository.go
@@ -1,7 +1,13 @@
 package repositories
 
 import (
+	"context"
+	"time"
+
 	"github.com/jackc/pgx/v5/pgxpool"
+	dataSource "github.com/princecee/go_chat/internal/db/data-source"
+	"github.com/princecee/go_chat/internal/models"
+	"github.com/princecee/go_chat/utils"
 )
 
 type userRepository struct {
@@ -12,8 +18,105 @@ func NewUserRepository(conn *pgxpool.Pool) *userRepository {
 	return &userRepository{conn}
 }
 
-func (r *userRepository) CreateUser() {}
-func (r *userRepository) GetUser()    {}
-func (r *userRepository) GetUsers()   {}
-func (r *userRepository) DeleteUser() {}
-func (r *userRepository) UpdateUser() {}
+func (r *userRepository) CreateUser(user *models.User, tx *pgxpool.Tx) error {
+	ds := dataSource.New(r.conn)
+	if tx != nil {
+		ds = ds.WithTx(tx)
+	}
+
+	_user, err := ds.CreateUser(context.Background(), dataSource.CreateUserParams{
+		LastName:  user.LastName,
+		FirstName: user.FirstName,
+		Email:     user.Email,
+	})
+	if err != nil {
+		return err
+	}
+
+	user.CreatedAt = _user.CreatedAt
+	user.UpdatedAt = _user.UpdatedAt
+	user.ID = _user.ID.String()
+
+	return nil
+}
+
+type GetUserParams struct {
+	ID    string
+	Email string
+}
+
+func (r *userRepository) GetUser(data GetUserParams, tx *pgxpool.Tx) (*models.User, error) {
+	ds := dataSource.New(r.conn)
+	if tx != nil {
+		ds = ds.WithTx(tx)
+	}
+
+	_user, err := ds.GetUser(context.Background(), dataSource.GetUserParams{
+		ID:    utils.StringToUUID(data.ID),
+		Email: data.Email,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &models.User{
+		ID:        _user.ID.String(),
+		FirstName: _user.FirstName,
+		LastName:  _user.LastName,
+		Email:     _user.Email,
+		CreatedAt: _user.CreatedAt,
+		UpdatedAt: _user.UpdatedAt,
+	}, nil
+}
+
+func (r *userRepository) GetUsers(tx *pgxpool.Tx) ([]*models.User, error) {
+	ds := dataSource.New(r.conn)
+	if tx != nil {
+		ds = ds.WithTx(tx)
+	}
+
+	_users, err := ds.GetUsers(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	users := []*models.User{}
+	for _, _user := range _users {
+		user := &models.User{
+			ID:        _user.ID.String(),
+			FirstName: _user.FirstName,
+			LastName:  _user.LastName,
+			Email:     _user.Email,
+			CreatedAt: _user.CreatedAt,
+			UpdatedAt: _user.UpdatedAt,
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func (r *userRepository) DeleteUser(userId string, tx *pgxpool.Tx) error {
+	ds := dataSource.New(r.conn)
+	if tx != nil {
+		ds = ds.WithTx(tx)
+	}
+
+	return ds.DeleteUser(context.Background(), utils.StringToUUID(userId))
+}
+
+func (r *userRepository) UpdateUser(user *models.User, tx *pgxpool.Tx) error {
+	ds := dataSource.New(r.conn)
+	if tx != nil {
+		ds = ds.WithTx(tx)
+	}
+
+	user.UpdatedAt = time.Now()
+	return ds.UpdateUser(context.Background(), dataSource.UpdateUserParams{
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+		UpdatedAt: user.UpdatedAt,
+		ID:        utils.StringToUUID(user.ID),
+	})
+}
--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/princecee/go_chat/internal/db/repositories"
+	"github.com/princecee/go_chat/internal/models"
 )
 
 type userService struct {
@@ -17,28 +18,38 @@ func NewUserService(conn *pgxpool.Pool) UserService {
 	}
 }
 
-func (s *userService) CreateUser() {}
+func (s *userService) CreateUser(user *models.User, tx *pgxpool.Tx) error {
+	return s.UserRepository.CreateUser(user, tx)
+}
 
-func (s *userService) GetUser() {}
+func (s *userService) GetUser(data repositories.GetUserParams, tx *pgxpool.Tx) (*models.User, error) {
+	return s.UserRepository.GetUser(data, tx)
+}
 
-func (s *userService) GetUsers() {}
+func (s *userService) GetUsers(tx *pgxpool.Tx) ([]*models.User, error) {
+	return s.UserRepository.GetUsers(tx)
+}
 
-func (s *userService) DeleteUser() {}
+func (s *userService) DeleteUser(userId string, tx *pgxpool.Tx) error {
+	return s.UserRepository.DeleteUser(userId, tx)
+}
 
-func (s *userService) UpdateUser() {}
+func (s *userService) UpdateUser(user *models.User, tx *pgxpool.Tx) error {
+	return s.UserRepository.UpdateUser(user, tx)
+}
 
 type UserService interface {
-	CreateUser()
-	GetUser()
-	GetUsers()
-	DeleteUser()
-	UpdateUser()
+	CreateUser(user *models.User, tx *pgxpool.Tx) error
+	GetUser(data repositories.GetUserParams, tx *pgxpool.Tx) (*models.User, error)
+	GetUsers(tx *pgxpool.Tx) ([]*models.User, error)
+	DeleteUser(userId string, tx *pgxpool.Tx) error
+	UpdateUser(user *models.User, tx *pgxpool.Tx) error
 }
 
 type UserRepository interface {
-	CreateUser()
-	GetUser()
-	GetUsers()
-	DeleteUser()
-	UpdateUser()
+	CreateUser(user *models.User, tx *pgxpool.Tx) error
+	GetUser(data repositories.GetUserParams, tx *pgxpool.Tx) (*models.User, error)
+	GetUsers(tx *pgxpool.Tx) ([]*models.User, error)
+	DeleteUser(userId string, tx *pgxpool.Tx) error
+	UpdateUser(user *models.User, tx *pgxpool.Tx) error
 }
